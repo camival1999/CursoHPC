43a21ffc66a6472f618bc4b5d8bb4c69c0c5d938
camival1999 <cvalenciaa@unal.edu.co>
2023-03-17 12:36:12 -0500

    Añadida semana 6 y ejercicio clase 2

---
+++ Semana_6/debug-example-1.cpp
@@ -0,0 +1,33 @@
+/*
+  ******************************* 
+  Debugging C++
+  Desarrollado por Camilo Andres Valencia Acevedo para el curso de Intro. al HPC 2023-1 
+  Semana 6, Clase 2 
+  ******************************* 
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+  // declare variables
+  const int N = 10;
+  double * array;
+  //reserve memory
+  array = new double [N];
+  // initialise array
+  for (int ii = 0; ii < N; ++ii) {
+    
+	  //std::cout"ii antes: "<<ii<<std:endl;
+	  array[ii] = 2*(++ii); // != 2*(ii++) ?
+	  //std::cout"ii despues: "<<ii<<std:endl;
+  }
+  // print some values
+  int idx = 2;
+  std::cout << array[idx] << std::endl;
+  idx = 10;
+  std::cout << array[idx] << std::endl; 
+
+  return 0;
+}
