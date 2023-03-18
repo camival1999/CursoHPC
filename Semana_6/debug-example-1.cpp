b122c1f94ca82004160d09fc40bada0a21800bc7
camival1999 <cvalenciaa@unal.edu.co>
2023-03-18 09:42:31 -0500

    Semana 6 Clase 2 - Error Memory leak

--- Semana_6/debug-example-1.cpp
+++ Semana_6/debug-example-1.cpp
@@ -13,15 +13,15 @@ int main(int argc, char **argv)
 {
   // declare variables
   const int N = 10;
-  double * array[N] = NULL;
+  double * array;
   //reserve memory
   array = new double [N];
   // initialise array
   for (int ii = 0; ii < N; ++ii) {
 
-	  //std::cout<<"ii antes: " << ii << std::endl;
+	  std::cout<<"ii antes: " << ii << std::endl;
 	  array[ii] = 2*(++ii); // != 2*(ii++) ?
-	  //std::cout<<"ii despues: "<< (ii) << std::endl;
+	  std::cout<<"ii despues: "<< (ii) << std::endl;
   }
   // print some values
   int idx = 2;
